7d87f877cd14637ccc294e7e81294da1a1c851ed
Alex Kagy <xelaygak@gmail.com>
2024-02-28 20:03:27 -0500

    dddewd

--- KhanAcademyProjects/sketch5.js
+++ KhanAcademyProjects/sketch5.js
@@ -4,13 +4,13 @@ let sketch5 = function(s) {
         s.createCanvas(s.windowWidth, 500);
     };
     s.draw = function(){
-        s.stroke(random(255), random(255), random(255));
+        s.stroke(s.random(255), s.random(255), s.random(255));
         s.strokeWeight(5);
         s.line(s.pmouseX, s.pmouseY, s.mouseX, s.mouseY);
 
     };
     s.mousePressed = function(){
-        s.fill(random(255), random(255), random(255));
+        s.fill(s.random(255), s.random(255), s.random(255));
         s.ellipse(s.mouseX, s.mouseY, 50, 50);
     };
 };
