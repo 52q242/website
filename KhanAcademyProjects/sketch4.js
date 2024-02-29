5ac67bac37de27a0faa13597f8687f214a0f1780
Alex Kagy <xelaygak@gmail.com>
2024-02-28 19:27:44 -0500

    swapping colors

--- KhanAcademyProjects/sketch4.js
+++ KhanAcademyProjects/sketch4.js
@@ -7,10 +7,10 @@ let sketch4 = function(apple) {
       apple.draw =function() {
         apple.background(255);
         apple.noStroke();
-        apple.fill(224, 90, 90);
+        apple.fill(48, 130, 31);
         apple.ellipse(150, 200, 150, 150);
         apple.ellipse(212, 200, 150, 150);
-        apple.fill(48, 130, 31);
+        apple.fill(224, 90, 90);
         apple.rect(176, 103, 12, 32); 
         apple.fill(255);
         apple.ellipse(apple.mouseX, apple.mouseY, 60, 60);
