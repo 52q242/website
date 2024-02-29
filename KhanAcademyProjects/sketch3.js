36a83979bcb1b96acd8dbd739b6792740a4d3bbd
Alex Kagy <xelaygak@gmail.com>
2024-02-29 11:14:40 -0500

    Stuff

--- KhanAcademyProjects/sketch3.js
+++ KhanAcademyProjects/sketch3.js
@@ -1,17 +1,24 @@
+let howBig = 30; // Define initial size
+
+let sketch3 = function(name) {
+  name.setup = function() {
+    name.createCanvas(400, 400);
+    name.fill(92, 24, 219);
+  };
+
+  name.draw = function() {
+    howBig += 0.5; // Increase size over time
+    name.textSize(howBig);
+    name.background(0, 238, 255);
+    let myName = "Alex"; // Define your name
+    name.text(myName, name.mouseX, name.mouseY); // Display name at mouse position
+  };
+};
+
+let myP5 = new p5(sketch3);
+ 
 
 
-let howBig = 30;
 
-function setup() {
-  createCanvas(400, 400);
-  fill(92, 24, 219);
-}
 
-function draw() {
-  howBig += 0.5;
-  textSize(howBig);
-  background(0, 238, 255);
-  let myName = "Alex";
-  let message = myName + "!!!";
-  text(message, mouseX, mouseY);
-}
+
