a413cb32973441553be0295f4c0f545b562180c5
Alex Kagy <xelaygak@gmail.com>
2024-02-28 20:11:40 -0500

    edited setup

--- KhanAcademyProjects/sketch5.js
+++ KhanAcademyProjects/sketch5.js
@@ -2,10 +2,12 @@
 let sketch5 = function(s) {
     s.setup = function() {
         s.createCanvas(s.windowWidth, 500);
+        s.stroke(255);
+        s.fill(255,255,255,100);
+        rect(0,0,s.windowWidth, 500);
 
     };
     s.draw = function(){
-        s.backround(255);
         s.stroke(s.random(255), s.random(255), s.random(255));
         s.strokeWeight(5);
         s.line(s.pmouseX, s.pmouseY, s.mouseX, s.mouseY);
