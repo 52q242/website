c39c630ef2f84c8bf7bbe3cb2775f87d04fea018
Alex Kagy <xelaygak@gmail.com>
2024-02-29 14:44:53 -0500

    ded

--- KhanAcademyProjects/sketch3.js
+++ KhanAcademyProjects/sketch3.js
@@ -11,19 +11,16 @@ let sketch3 = function(name) {
     name.textSize(howBig);
     name.background(0, 238, 255);
     let myName = "Alex"; 
-    // check if the mouse is pressed and inside the canvas
-    if (name.mouseIsPressed && name.mouseX >= 0 && name.mouseX <= name.width && name.mouseY >= 0 && name.mouseY <= name.height) {
-      // update the name's position to the mouse position
+  
+    if (name.mouseX >= 0 && name.mouseX <= name.width && name.mouseY >= 0 && name.mouseY <= name.height) {
+
       name.text(myName, name.mouseX, name.mouseY); 
     } else {
-      // keep the name's position in the center of the canvas
-      name.text(myName, name.width / 2, name.height / 2);
+
+      name.text(myName, name.mouseX, name.mouseY);
     }
   };
 };
-
-let myP5 = new p5(sketch3);
- 
 
 
 
