aa071321781116b4471f17edfc1ce6b97c7d6832
Alex Kagy <xelaygak@gmail.com>
2024-02-28 19:14:11 -0500

    Updated instantiation for tomato

--- KhanAcademyProjects/sketch.js
+++ KhanAcademyProjects/sketch.js
@@ -1,20 +1,20 @@
-function setup() {
-  createCanvas(400, 400);
-}
 
-function draw() {
-  background(255);
-  
-  // tomato
-  noStroke();
-  fill(224, 90, 90);
-  ellipse(150, 200, 150, 150);
-  ellipse(212, 200, 150, 150);
-  
-  // stem
-  fill(48, 130, 31);
-  rect(176, 103, 12, 32); 
-  
-  fill(255);
-  ellipse(mouseX, mouseY, 60, 60);
-}
+let sketch1 = function(tomato) {
+ 
+tomato.setup = function() {
+  tomato.createCanvas(400, 400);
+  };
+  tomato.draw =function() {
+    tomato.background(255);
+    tomato.noStroke();
+    tomato.fill(224, 90, 90);
+    tomato.ellipse(150, 200, 150, 150);
+    tomato.ellipse(212, 200, 150, 150);
+    tomato.fill(48, 130, 31);
+    tomato.rect(176, 103, 12, 32); 
+    tomato.fill(255);
+    tomato.ellipse(mouseX, mouseY, 60, 60);
+  };
+};
+
+let tp5 = new p5(sketch1);
